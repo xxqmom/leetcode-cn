ba9c53160a3c8dd21da78d99bc38c9eae8c0add0
xxqmom <60057588+xxqmom@users.noreply.github.com>
2022-03-26 20:37:34 +0800

    Time: 3 ms (63.75%), Space: 8.8 MB (73.14%) - LeetHub

---
+++ remove-element/remove-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int slow = 0, fast = 0;
+        while(fast < nums.size()){
+            if(nums[fast]!=val){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        nums.erase(nums.begin()+slow, nums.end());
+        return nums.size();
+    }
+};
